fbc06fc3d0e35f2ed67282c9569e13dcfb31908d
tenondvpn <jhsx5679@gmail.com>
2025-09-02 06:51:17 +0800

    fix

--- dags/settings.py
+++ dags/settings.py
@@ -60,10 +60,26 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,  # 自动刷新refresh token
     'BLACKLIST_AFTER_ROTATION': True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),  # 访问 token 有效期
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),   # 刷新 token 有效期
+    "ROTATE_REFRESH_TOKENS": False,                # 是否在刷新时生成新 token
+    "BLACKLIST_AFTER_ROTATION": False,             # 刷新后是否将旧 token 列入黑名单
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": "你的密钥",  # 建议使用 SECRET_KEY
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
 }
 
 

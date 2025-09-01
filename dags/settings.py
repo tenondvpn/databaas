38b279f73b77e8d81940a39e1fa562eea112a724
tenondvpn <jhsx5679@gmail.com>
2025-09-02 00:05:39 +0800

    fix

--- dags/settings.py
+++ dags/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'horae',
     'picker',
     'pyplugin',

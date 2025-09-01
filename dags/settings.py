57cb998ef5bb05f4e2c542af5b9401b6c29fd80c
tenondvpn <jhsx5679@gmail.com>
2025-09-02 05:22:14 +0800

    fix

--- dags/settings.py
+++ dags/settings.py
@@ -57,9 +57,6 @@ REST_FRAMEWORK = {
 }
 
 JWT_AUTH = {
-    'JWT_SECRET_KEY': SECRET_KEY,
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_ALLOW_REFRESH': True,
     'JWT_EXPIRATION_DELTA': timedelta(days=1),
     'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
 }

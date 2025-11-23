debd4ae249d97f87555925c1e46d4403d67b5a30
tenondvpn <jhsx5679@gmail.com>
2025-11-24 00:21:28 +0800

    fix

--- dags/views.py
+++ dags/views.py
@@ -31,7 +31,7 @@ def rest_register(request):
     username = request.data.get('username')
     password = request.data.get('password')
     email = request.data.get('email')
-    phone = request.data.get('phone')
+    dingding = request.data.get('phone')
 
     if not username or not password:
         return Response({'message': '用户名和密码不能为空'}, status=status.HTTP_400_BAD_REQUEST)
@@ -44,6 +44,11 @@ def rest_register(request):
             except:
                 pass
             token, created = Token.objects.get_or_create(user=user)
+            try:
+                user_info = UserInfo(userid=user.id, email=email, dingding=dingding)
+                user_info.save()
+            except:
+                pass
             return Response({'token': token.key, 'message': '登录成功', 'created': created}, status=status.HTTP_200_OK)
         else:
             return Response({'message': '用户名或密码错误: ' + username}, status=status.HTTP_401_UNAUTHORIZED)
@@ -51,6 +56,11 @@ def rest_register(request):
     user = User.objects.create_user(username=username, password=password, email=email)
     try:
         user.auth_token.delete()
+    except:
+        pass
+    try:
+        user_info = UserInfo(userid=user.id, email=email, dingding=dingding)
+        user_info.save()
     except:
         pass
     token, created = Token.objects.get_or_create(user=user)
